8bf635fd1b9a702914b024034bb9ccef9b368645
lele <1173337058@qq.com>
2020-12-05 15:58:50 +0800

    package com.lel;
    import java.io.FileInputStream;
    import java.io.FileNotFoundException;
    import java.util.ArrayList;
    import java.util.Date;
    
    public class hellowworld {
    
        public static void main(String[] args) {
    //sjdfldsjfk fdsjfl
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            //region Description
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            //endregion
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
    
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
    
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
    
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
            System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
    
            ArrayList list1 = new ArrayList();
            Date date = new Date();
    
        }
    
        public static void method() {
    
            try {
                FileInputStream fis =new FileInputStream("dsfsd.txt") ;
            } catch (FileNotFoundException e) {
                e.printStackTrace();
            } finally {
            }
    
        }
    
    }

---
+++ src/main/java/com/lel/hellowworld.java
@@ -0,0 +1,90 @@
+package com.lel;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class hellowworld {
+
+    public static void main(String[] args) {
+//sjdfldsjfk fdsjfl
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        //region Description
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        //endregion
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+
+
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+
+
+
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+        System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");System.out.println("dsfdsafsdafsdfsdafsdafsdafsd");
+
+
+
+
+
+        ArrayList list1 = new ArrayList();
+        Date date = new Date();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public static void method() {
+
+        try {
+            FileInputStream fis =new FileInputStream("dsfsd.txt") ;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
